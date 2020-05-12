8ee56eba8196af2772b957f9cf01a73394c31365
Łukasz Mazurkiewicz <mazurkiewicz_l@outlook.com>
2020-05-12 18:49:14 +0200

    ADD endpoints tests

--- server.js
+++ server.js
@@ -22,14 +22,16 @@ app.use((req, res) => {
 })
 
 // connects our backend code with the database
-mongoose.connect('mongodb://localhost:27017/companyDB', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost:27017/companyDB', { useNewUrlParser: true, useUnifiedTopology: true } );
 const db = mongoose.connection;
 
 db.once('open', () => {
-  console.log('Connected to the database');
+  //console.log('Connected to the database');
 });
 db.on('error', err => console.log('Error ' + err));
 
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
+const server = app.listen('8000', () => {
+  //console.log('Server is running on port: 8000');
 });
+
+module.exports = server;
